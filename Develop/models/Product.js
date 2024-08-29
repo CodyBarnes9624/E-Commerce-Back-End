96379b55e33c3e4915087d012354570b9e7542d1
CodyBarnes9624 <167149007+CodyBarnes9624@users.noreply.github.com>
2024-08-29 19:09:10 -0400

    set up models and routes

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -10,7 +10,49 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+        min: 0,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+      onDelete: 'SET NULL',
+    },
   },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product',
+  },
+  
   {
     sequelize,
     timestamps: false,
