0a119ca775880f82121abf6779e3663da5dfe920
CodyBarnes9624 <167149007+CodyBarnes9624@users.noreply.github.com>
2024-08-29 20:52:44 -0400

    fixed some routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
-    const tags = await Tag.findAll({
+    const allTags = await Tag.findAll({
       include: [{ model: Product }],
     });
     res.status(200).json(allTags);
@@ -20,7 +20,7 @@ router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
-    const tag = await Tag.findByPk(req.params.id, {
+    const singleTag = await Tag.findByPk(req.params.id, {
       include: [{ model: Product }],
     });
 
